04c68e5af7f82ce4b16630d750224d1595ccadf5
Pascal Monasse <pascal.monasse@enpc.fr>
2025-01-27 16:57:12 +0100

    Draw saddle level lines

---
+++ bilines.cpp
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * @file bilines.cpp
+ * @brief Display bilinear level lines.
+ * 
+ * (C) 2019, Pascal Monasse <pascal.monasse@enpc.fr>
+ */
+
+#include "lltree.h"
+#include "draw_curve.h"
+#include "fill_curve.h"
+#include "cmdLine.h"
+#include "io_png.h"
+#include <map>
+#include <cmath>
+
+struct color_t {
+    unsigned char r,g,b;
+    color_t(): r(255), g(255), b(255) {}
+    color_t(unsigned char r0, unsigned char g0, unsigned char b0)
+    :r(r0),g(g0),b(b0) {}
+};
+
+const color_t WHITE(255,255,255);
+const color_t GREEN(0,255,0);
+
+/// Compute histogram of level at pixels at the border of the image.
+static void histogram(unsigned char* im, size_t w, size_t h, size_t histo[256]){
+    size_t j;
+    for(j=0; j<w; j++) // First line
+        ++histo[im[j]];
+    for(size_t i=1; i+1<h; i++) { // All lines except first and last
+        ++histo[im[j]];  // First pixel of line
+        j+= w-1;
+        ++histo[im[j++]]; // Last pixel of line
+    }
+    for(; j<w*h; j++) // Last line
+        ++histo[im[j]];    
+}
+
+/// Put pixels at border of image to value \a v.
+static void put_border(unsigned char* im, size_t w, size_t h, unsigned char v) {
+    size_t j;
+    for(j=0; j<w; j++)
+        im[j] = v;
+    for(size_t i=1; i+1<h; i++) {
+        im[j] = v;
+        j+= w-1;
+        im[j++] = v;
+    }
+    for(; j<w*h; j++)
+        im[j] = v;
+}
+
+/// Set all pixels at border of image to their median level.
+static unsigned char fill_border(unsigned char* im, size_t w, size_t h) {
+    size_t histo[256] = {0}; // This puts all values to zero
+    histogram(im, w, h, histo);
+    size_t limit=w+h-2; // Half number of pixels at border
+    size_t sum=0;
+    int i=-1;
+    while((sum+=histo[++i]) < limit);
+    put_border(im,w,h, (unsigned char)i);
+    return (unsigned char)i;
+}
+
+/// Main procedure for curvature microscope.
+int main(int argc, char** argv) {
+    CmdLine cmd; cmd.prefixDoc = "\t";
+    //    cmd.add( make_option('q',qstep).doc("Quantization step (integer)") );
+    cmd.process(argc, argv);
+    if(argc!=3) {
+        std::cerr << "Usage: " << argv[0]
+                  << " [options] in.png out.png" << std::endl;
+        std::cerr << "Option:\n" << cmd;
+        return 1;
+    }
+
+    size_t w, h;
+    unsigned char* in = io_png_read_u8_gray(argv[1], &w, &h);
+    if(! in) {
+        std::cerr << "Error reading as PNG image: " << argv[1] << std::endl;
+        return 1;
+    }
+    fill_border(in, w, h); // Background gray of output
+
+    // Extract level lines
+    LLTree tree(in, (int)w, (int)h, 0);
+    free(in);
+    std::cout << tree.nodes().size() << " level lines." << std::endl;
+
+    // Draw level lines
+    color_t* out = new color_t[w*h];
+    for(LLTree::iterator it=tree.begin(); it!=tree.end(); ++it)
+        draw_curve(it->ll->line,GREEN, out+0*w*h,(int)w,(int)h);
+
+    // Output image
+    if(io_png_write_u8(argv[2], (unsigned char*)out, (int)w, (int)h, 3)!=0) {
+        std::cerr << "Error writing image file " << argv[2] << std::endl;
+        return 1;
+    }
+    delete [] out;
+
+    return 0;
+}
---
+++ draw_curve.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * @file draw_curve.h
+ * @brief Draw a curve in an image
+ * 
+ * (C) 2011-2014, 2019, Pascal Monasse <pascal.monasse@enpc.fr>
+ */
+
+#ifndef DRAW_CURVE_H
+#define DRAW_CURVE_H
+
+#include "levelLine.h"
+
+template <typename T>
+void draw_curve(const std::vector<Point>& curve, T v, T* im, int w, int h);
+
+#include "draw_curve.cpp"
+
+#endif
